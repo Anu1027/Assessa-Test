027314353ad29099c9804b95c93dea30d577c63f
Anuradha Somisetti <163394044+Anu1027@users.noreply.github.com>
2025-11-12 18:12:45 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -17,7 +17,8 @@ const allowedOrigins = [
   "http://localhost:3000",
   "http://localhost:5000",
   "https://www.assessaai.com",
-  "https://assessaai.com"
+  "https://assessaai.com",
+  "https://test.assessaai.com"
 ];
 
 const corsOptions = {
